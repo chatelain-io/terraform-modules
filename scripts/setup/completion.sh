23ef9cbf533267d87388c1f4c620d4453e368978
Alexandre Chatelain <achatelain@artm.quebec>
2025-09-29 08:03:01 -0400

    feat: add shell completion script for various tools

---
+++ scripts/setup/completion.sh
@@ -0,0 +1,13 @@
+source <(kubelogin completion bash)
+source <(gh completion bash)
+source <(kind completion bash)
+source <(kubectl completion bash)
+source <(yq completion bash)
+source "$(brew --prefix)/etc/bash_completion.d/az"
+eval "$(npm completion)"
+eval "$(uv generate-shell-completion bash)"
+eval "$(uvx --generate-shell-completion bash)"
+eval "$(direnv hook bash)"
+source <(chainsaw completion bash)
+source <(devbox completion bash)
+source <(npm completion)
